a02e456b32c00e6db66297f6e0f0c762d282b469
ricobach <40454890+ricobach@users.noreply.github.com>
2026-08-20 15:05:05 +0200

    Add integration constants

---
+++ custom_components/ha_remote_bridge/const.py
@@ -0,0 +1,9 @@
+"""Constants for HA Remote Bridge."""
+
+DOMAIN = "ha_remote_bridge"
+
+CONF_VERIFY_SSL = "verify_ssl"
+DEFAULT_VERIFY_SSL = True
+
+PLATFORMS = ["sensor"]
+API_BASE = "/api/ha_remote_bridge"
